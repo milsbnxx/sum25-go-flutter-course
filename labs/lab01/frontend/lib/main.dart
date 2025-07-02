610e8f46d3a7a9f896288745993ce53844dd3cff
milsbnxx <sabina120207@gmail.com>
2025-07-02 13:47:29 +0300

    Update main.dart

--- labs/lab01/frontend/lib/main.dart
+++ labs/lab01/frontend/lib/main.dart
@@ -1,8 +1,7 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
-import 'counter_app.dart';
-import 'profile_card.dart';
-import 'registration_form.dart';
+import 'package:frontend/counter_app.dart';
+import 'package:frontend/profile_card.dart';
+import 'package:frontend/registration_form.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Lab 01 Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -29,40 +28,34 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Lab 01 Demo'),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            Text(
-              'Profile Card Example',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Lab 01 Demo'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Profile'),
+              Tab(text: 'Counter'),
+              Tab(text: 'Register'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Center(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.all(16.0),
+                child: ProfileCard(
+                  name: 'John Doe',
+                  email: 'john.doe@example.com',
+                  age: 25,
+                  avatarUrl: null,
+                ),
+              ),
             ),
-            SizedBox(height: 8),
-            ProfileCard(
-              name: 'Sabina Yamilova',
-              email: 's.yamilova@innopolis.university',
-              age: 30,
-              avatarUrl: 'https://example.com/avatar.jpg',
-            ),
-            SizedBox(height: 24),
-            Text(
-              'Counter App',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
             CounterApp(),
-            SizedBox(height: 24),
-            Text(
-              'Registration Form ',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
             RegistrationForm(),
           ],
         ),

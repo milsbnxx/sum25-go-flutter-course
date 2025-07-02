58182d6a0a497330461f21d7ab55ad78390c9c00
milsbnxx <sabina120207@gmail.com>
2025-07-02 13:44:07 +0300

    Update calculator.go

--- labs/lab01/backend/calculator/calculator.go
+++ labs/lab01/backend/calculator/calculator.go
@@ -5,24 +5,19 @@ import (
 	"strconv"
 )
 
-
 var ErrDivisionByZero = errors.New("division by zero")
-
 
 func Add(a, b float64) float64 {
 	return a + b
 }
 
-
 func Subtract(a, b float64) float64 {
 	return a - b
 }
 
-
 func Multiply(a, b float64) float64 {
 	return a * b
 }
-
 
 func Divide(a, b float64) (float64, error) {
 	if b == 0 {
@@ -31,12 +26,14 @@ func Divide(a, b float64) (float64, error) {
 	return a / b, nil
 }
 
-
 func StringToFloat(s string) (float64, error) {
 	return strconv.ParseFloat(s, 64)
 }
 
 
 func FloatToString(f float64, precision int) string {
+	if precision < 0 {
+		precision = -1
+	}
 	return strconv.FormatFloat(f, 'f', precision, 64)
 }
